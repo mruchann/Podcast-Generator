22fbaf1d474c12568460c38aa6056ca13f3a2f94
Mehmet Rüçhan Yavuzdemir <ruchan.yavuzdemir@icloud.com>
2024-07-05 23:18:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+git pull
+
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
